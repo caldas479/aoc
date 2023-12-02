4a51ab72f33adf239938844f25d43e4591e38e25
Tiago Caldas <tiago.rodrigues.caldas@gmail.com>
2023-12-02 14:10:51 +0000

    day 2

--- 2023/02/sol.py
+++ 2023/02/sol.py
@@ -12,16 +12,21 @@ for id, game in enumerate(lines):
     game = game.strip().split(':')
     sets = game[1].split(';')
     valid = True
+    r = b = g = 0
     for play in sets:
         moves = play.strip().split(',')
-        r = b = g = 0
+        
         for move in moves:
             move = move.strip().split(' ')
             n = int(move[0])
             color = move[1]
-            if elf_bag[color] < n:
-                valid = False
-    if valid:
-        ans += id + 1
+            if color == 'red':
+                r = max(r,n)
+            elif color == 'blue':
+                b = max(b,n)
+            else:
+                g = max(g,n)
+        
+    ans += r*g*b
 
 print(ans)
