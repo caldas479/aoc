02f7443344f9a003040eeb94e1e71ce3c1cf21a9
Tiago Caldas <tiago.rodrigues.caldas@gmail.com>
2023-12-05 14:12:24 +0000

    part 1

--- 2023/05/sol.py
+++ 2023/05/sol.py
@@ -14,10 +14,8 @@ for line in lines:
             if s in translate:
                 seeds[i] = translate[s]
         translate = {}
-         
     elif line[0].isdigit():
         nums = [int(n) for n in line.split()]
-        i = 0
         for i,s in enumerate(seeds):
             if nums[1] <= s < nums[1] + nums[2]:
                 translate[s] = nums[0] + (seeds[i] - nums[1])
@@ -26,7 +24,4 @@ for i,s in enumerate(seeds):
     if s in translate:
         seeds[i] = translate[s]
 
-ans = 100000000000
-for s in seeds:
-    ans = min(ans,s)
-print(ans)
+print(min(seeds))
