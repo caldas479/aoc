24e709883df9eb7ae3bdc8e60d42f30430bae510
Tiago Caldas <tiago.rodrigues.caldas@gmail.com>
2023-12-05 14:19:18 +0000

    part 1

--- 2023/05/sol.py
+++ 2023/05/sol.py
@@ -1,27 +1,20 @@
 import sys
 
 data = open(sys.argv[1]).read().strip()
-lines = data.split('\n')
-
+lines = [l for l in data.split('\n') if l!='']
 seeds = lines[0].split(':')[1]
 seeds = [int(n) for n in seeds.split()]
-
-lines = lines[1:]
 translate =  {}
-for line in lines:
-    if not len(line):
-        for i,s in enumerate(seeds):
-            if s in translate:
-                seeds[i] = translate[s]
-        translate = {}
-    elif line[0].isdigit():
+for line in lines[1:]:       
+    if line[0].isdigit():
         nums = [int(n) for n in line.split()]
         for i,s in enumerate(seeds):
             if nums[1] <= s < nums[1] + nums[2]:
                 translate[s] = nums[0] + (seeds[i] - nums[1])
-
-for i,s in enumerate(seeds):
-    if s in translate:
-        seeds[i] = translate[s]
+    else:
+        for i,s in enumerate(seeds):
+            if s in translate:
+                seeds[i] = translate[s]
+        translate = {}
 
 print(min(seeds))
