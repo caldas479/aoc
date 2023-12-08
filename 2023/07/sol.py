af71778244f14af6340156282893b195a0acb217
Tiago Caldas <tiago.rodrigues.caldas@gmail.com>
2023-12-08 16:26:49 +0000

    day7

--- 2023/07/sol.py
+++ 2023/07/sol.py
@@ -3,29 +3,21 @@ import sys
 
 lines = open(sys.argv[1]).read().strip().split('\n')
 hands = []
-
-sameTypeHands = defaultdict(list)
-
-values = {'A': 14, 'K': 13, 'Q':12, 'J': 11, 'T': 10}
-
-def count_cards(hand):
-    count = set()
-    for k, v in cards.items():
-        count.add(v)
-    return count
+values = {'A': 14, 'K': 13, 'Q':12, 'J': 1, 'T': 10}
 
 for line in lines:
     hand, bid = line.split(' ')
     cards = {}
-    val = 0
+    h = []
     for card in hand:
         if card in values:
             card = values[card]
+        card = int(card)
         if card not in cards:
             cards[card] = 0
         cards[card] += 1
-    print(cards)
-    count = count_cards(cards)
+        h += [card]     
+    count = cards.values()
     if len(cards) == 1:
         t = 7
     elif len(cards) == 2:     
@@ -35,16 +27,13 @@ for line in lines:
     elif len(cards) == 4:
         t = 2
     else:
-        t = 1
-    cards['bid'] = int(bid) 
-    cards['type'] = t
-    cards['val'] = val
-    hands += [cards]
+        t = 1   
+    hands += [(t,h,int(bid))]
 
-sorted_hands = sorted(hands, key=lambda x: (x['type'], x['val']))
+sorted_hands = sorted(hands, key=lambda x: (x[0],x[1]))
 
 ans = 0
 for i,hand in enumerate(sorted_hands):
-    ans += (i+1)*hand['bid']
+    ans += (i+1)*hand[2]
 
 print(ans)
