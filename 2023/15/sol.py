aa0cadce2bd375bd34b722d3e606f5adf5fcc823
Tiago Caldas <tiago.rodrigues.caldas@gmail.com>
2023-12-20 02:16:36 +0000

    day15

--- 2023/15/sol.py
+++ 2023/15/sol.py
@@ -1,7 +1,7 @@
 import sys
 
 data = open(sys.argv[1]).read().strip().split('\n')
-
+p1 = p2 = 0
 def hash(s):
     curr = 0
     for ch in s:
@@ -9,8 +9,24 @@ def hash(s):
         curr *= 17
         curr %= 256
     return curr
-ans = 0
+boxes = [[] for _ in range(256)]
+focal = {}
 for step in data[0].split(','):
-    ans += hash(step)
-
-print(ans)
+    p1 += hash(step)
+    if "-" in step:
+        label = step[:-1]
+        i = hash(label)
+        if label in boxes[i]:
+            boxes[i].remove(label)
+    else:
+        label, l = step.split("=")
+        l = int(l)
+        i = hash(label)
+        if label not in boxes[i]:
+            boxes[i].append(label)
+        focal[label]  = l
+    
+for ib, box in enumerate(boxes,1):
+    for il, label in enumerate(box,1):
+        p2 += ib*il*focal[label]
+print(p1,p2)
