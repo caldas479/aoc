2f0b13378b458c18ffee946f9999438a6d1fc58a
Tiago Caldas <tiago.rodrigues.caldas@gmail.com>
2023-12-22 19:55:17 +0000

    day17

--- 2023/17/sol.py
+++ 2023/17/sol.py
@@ -6,32 +6,30 @@ data = open(sys.argv[1]).read().strip().split('\n')
 grid = [[int(ch) for ch in row] for row in data]
 
 
+q = [(0,0,0,0,0,0)]
 seen = set()
-pq = [(0, 0, 0, 0, 0, 0)]
 
-while pq:
-    hl, r, c, dr, dc, n = heappop(pq)
-    
-    if r == len(grid) - 1 and c == len(grid[0]) - 1:
-        print(hl)
-        break
+while q:
+	h,r,c,dr,dc,n = heappop(q)
+	if r == len(grid)-1 and c == len(grid[0])-1:
+		print(h)
+		break
 
-    if (r, c, dr, dc, n) in seen:
-        continue
+	if (r,c,dr,dc,n) in seen:
+		continue
 
-    seen.add((r, c, dr, dc, n))
-    
-    if n < 3 and (dr, dc) != (0, 0):
-        nr = r + dr
-        nc = c + dc
-        if 0 <= nr < len(grid) and 0 <= nc < len(grid[0]):
-            heappush(pq, (hl + grid[nr][nc], nr, nc, dr, dc, n + 1))
+	seen.add((r,c,dr,dc,n))
 
-    for ndr, ndc in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
-        if (ndr, ndc) != (dr, dc) and (ndr, ndc) != (-dr, -dc):
-            nr = r + ndr
-            nc = c + ndc
-            if 0 <= nr < len(grid) and 0 <= nc < len(grid[0]):
-                heappush(pq, (hl + grid[nr][nc], nr, nc, ndr, ndc, 1))
-
+	if n<10 and (dr,dc) != (0,0):
+		rr = r+dr
+		cc = c+dc
+		if 0<=rr<len(grid) and 0<=cc<len(grid[0]):
+			heappush(q,(h+grid[rr][cc],rr,cc,dr,dc,n+1))
+	if n==0 or n>=4:
+		for drr, dcc in [(0,1),(1,0),(0,-1),(-1,0)]:
+			if (drr,dcc) != (dr,dc) and (drr,dcc) != (-dr,-dc):
+				rr = r+drr
+				cc = c+dcc
+				if 0<=rr<len(grid) and 0<=cc<len(grid[0]):
+					heappush(q,(h+grid[rr][cc],rr,cc,drr,dcc,1))
 
